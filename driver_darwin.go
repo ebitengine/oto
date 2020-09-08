e37cac2d67bf456bf3a1d8b82da405a2c7d18308
Hajime Hoshi <hajimehoshi@gmail.com>
2020-09-08 12:59:44 +0900

    misspelling

--- driver_darwin.go
+++ driver_darwin.go
@@ -171,7 +171,7 @@ func audioQueueStartWithRetrying(audioQueue C.AudioQueueRef) error {
 		}
 		return fmt.Errorf("oto: AudioQueueStart failed: %d", osstatus)
 	}
-	return fmt.Errorf("oto: AudioQueuStart has been failing: %d", C.AVAudioSessionErrorCodeCannotStartPlaying)
+	return fmt.Errorf("oto: AudioQueueStart has been failing: %d", C.AVAudioSessionErrorCodeCannotStartPlaying)
 }
 
 //export oto_render
