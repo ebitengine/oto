851a82cb8f22ccb6f8b186d23bb9931f7ef08869
Hajime Hoshi <hajimehoshi@gmail.com>
2021-08-23 01:56:56 +0900

    Add go:build comments for Go 1.17

--- driver_macos.m
+++ driver_macos.m
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build darwin && !ios && !js
 // +build darwin,!ios,!js
 
 #import <AppKit/AppKit.h>
