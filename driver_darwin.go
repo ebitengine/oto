6455a13f7c2d763030e7402d89257522b016d157
Hajime Hoshi <hajimehoshi@gmail.com>
2021-10-22 15:06:18 +0900

    darwin: Bug fix: Make Suspend and Resume concurrent-safe
    Closes #153

--- driver_darwin.go
+++ driver_darwin.go
@@ -159,6 +159,9 @@ func (c *context) appendBuffer(buf32 []float32) {
 }
 
 func (c *context) Suspend() error {
+	c.cond.L.Lock()
+	defer c.cond.L.Unlock()
+
 	if osstatus := C.AudioQueuePause(c.audioQueue); osstatus != C.noErr {
 		return fmt.Errorf("oto: AudioQueuePause failed: %d", osstatus)
 	}
@@ -166,6 +169,8 @@ func (c *context) Suspend() error {
 }
 
 func (c *context) Resume() error {
+	c.cond.L.Lock()
+	defer c.cond.L.Unlock()
 try:
 	if osstatus := C.AudioQueueStart(c.audioQueue, nil); osstatus != C.noErr {
 		const AVAudioSessionErrorCodeSiriIsRecording = 0x73697269 // 'siri'
