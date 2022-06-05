18ccea22b07a31262b9c652f4ea63188dda5be56
Hajime Hoshi <hajimehoshi@gmail.com>
2022-06-06 07:04:00 +0900

    Revert commits
    This reverts these commits
    * 42e404f878ea57b5d6f8816e295a7e80c027eb66
    * cc4af4f939d31547907a13f6388fd1400ed44799
    
    Reason: see https://github.com/hajimehoshi/ebiten/issues/2118#issuecomment-1146890541

--- driver_unix.go
+++ driver_unix.go
@@ -24,8 +24,6 @@ import "C"
 
 import (
 	"fmt"
-	"os/exec"
-	"strings"
 	"sync"
 	"time"
 	"unsafe"
@@ -49,41 +47,8 @@ type context struct {
 
 var theContext *context
 
-func alsaError(name string, err C.int) error {
-	return fmt.Errorf("oto: ALSA error at %s: %s", name, C.GoString(C.snd_strerror(err)))
-}
-
-func deviceCandidates() []string {
-	cmd := exec.Command("arecord", "-L")
-	out, err := cmd.Output()
-	if err != nil {
-		return []string{"default"}
-	}
-
-	var devices []string
-	lines := strings.Split(string(out), "\n")
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
-		if line[0] == ' ' || line[0] == '\t' {
-			continue
-		}
-		device := strings.TrimSpace(line)
-		if device == "null" {
-			continue
-		}
-		if device == "default" {
-			continue
-		}
-		if device == "hw" || strings.HasPrefix(device, "hw:") {
-			continue
-		}
-		devices = append(devices, device)
-	}
-
-	devices = append([]string{"default"}, devices...)
-	return devices
+func alsaError(err C.int) error {
+	return fmt.Errorf("oto: ALSA error: %s", C.GoString(C.snd_strerror(err)))
 }
 
 func newContext(sampleRate, channelNum, bitDepthInBytes int) (*context, chan struct{}, error) {
@@ -100,32 +65,10 @@ func newContext(sampleRate, channelNum, bitDepthInBytes int) (*context, chan str
 	theContext = c
 
 	// Open a default ALSA audio device for blocking stream playback
-	type openError struct {
-		device string
-		err    C.int
-	}
-	var openErrs []openError
-	var found bool
-
-	for _, name := range deviceCandidates() {
-		cname := C.CString(name)
-		defer C.free(unsafe.Pointer(cname))
-		if err := C.snd_pcm_open(&c.handle, cname, C.SND_PCM_STREAM_PLAYBACK, 0); err < 0 {
-			openErrs = append(openErrs, openError{
-				device: name,
-				err:    err,
-			})
-			continue
-		}
-		found = true
-		break
-	}
-	if !found {
-		var msgs []string
-		for _, e := range openErrs {
-			msgs = append(msgs, fmt.Sprintf("%q: %s", e.device, C.GoString(C.snd_strerror(e.err))))
-		}
-		return nil, nil, fmt.Errorf("oto: ALSA error at snd_pcm_open: %s", strings.Join(msgs, ", "))
+	cname := C.CString("default")
+	defer C.free(unsafe.Pointer(cname))
+	if err := C.snd_pcm_open(&c.handle, cname, C.SND_PCM_STREAM_PLAYBACK, 0); err < 0 {
+		return nil, nil, alsaError(err)
 	}
 
 	periodSize := C.snd_pcm_uframes_t(1024)
@@ -152,32 +95,32 @@ func (c *context) alsaPcmHwParams(sampleRate, channelNum int, bufferSize, period
 	defer C.free(unsafe.Pointer(params))
 
 	if err := C.snd_pcm_hw_params_any(c.handle, params); err < 0 {
-		return alsaError("snd_pcm_hw_params_any", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_access(c.handle, params, C.SND_PCM_ACCESS_RW_INTERLEAVED); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_access", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_format(c.handle, params, C.SND_PCM_FORMAT_FLOAT_LE); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_format", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_channels(c.handle, params, C.unsigned(channelNum)); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_channels", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_rate_resample(c.handle, params, 1); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_rate_resample", err)
+		return alsaError(err)
 	}
 	sr := C.unsigned(sampleRate)
 	if err := C.snd_pcm_hw_params_set_rate_near(c.handle, params, &sr, nil); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_rate_near", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_buffer_size_near(c.handle, params, bufferSize); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_buffer_size_near", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params_set_period_size_near(c.handle, params, periodSize, nil); err < 0 {
-		return alsaError("snd_pcm_hw_params_set_period_size_near", err)
+		return alsaError(err)
 	}
 	if err := C.snd_pcm_hw_params(c.handle, params); err < 0 {
-		return alsaError("snd_pcm_hw_params", err)
+		return alsaError(err)
 	}
 	c.supportsPause = C.snd_pcm_hw_params_can_pause(params) == 1
 	return nil
@@ -201,13 +144,13 @@ func (c *context) readAndWrite(buf32 []float32) bool {
 		if n == -C.EPIPE {
 			// Underrun or overrun occurred.
 			if err := C.snd_pcm_prepare(c.handle); err < 0 {
-				c.err.TryStore(alsaError("snd_pcm_prepare", err))
+				c.err.TryStore(alsaError(err))
 				return false
 			}
 			continue
 		}
 		if n < 0 {
-			c.err.TryStore(alsaError("snd_pcm_writei", C.int(n)))
+			c.err.TryStore(alsaError(C.int(n)))
 			return false
 		}
 		buf32 = buf32[int(n)*c.channelNum:]
@@ -226,13 +169,13 @@ func (c *context) Suspend() error {
 	c.suspended = true
 	if c.supportsPause {
 		if err := C.snd_pcm_pause(c.handle, 1); err < 0 {
-			return alsaError("snd_pcm_pause", err)
+			return alsaError(err)
 		}
 		return nil
 	}
 
 	if err := C.snd_pcm_drop(c.handle); err < 0 {
-		return alsaError("snd_pcm_drop", err)
+		return alsaError(err)
 	}
 	return nil
 }
@@ -252,7 +195,7 @@ func (c *context) Resume() error {
 
 	if c.supportsPause {
 		if err := C.snd_pcm_pause(c.handle, 0); err < 0 {
-			return alsaError("snd_pcm_pause", err)
+			return alsaError(err)
 		}
 		return nil
 	}
@@ -265,11 +208,11 @@ try:
 		}
 		if err == -C.ENOSYS {
 			if err := C.snd_pcm_prepare(c.handle); err < 0 {
-				return alsaError("snd_pcm_prepare", err)
+				return alsaError(err)
 			}
 			return nil
 		}
-		return alsaError("snd_pcm_resume", err)
+		return alsaError(err)
 	}
 	return nil
 }
