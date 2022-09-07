595bc3caefb1f6baf1ea60850a5007cae6770b14
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2022-09-06 23:37:05 -0400

    remove Cgo on darwin (#185)
    This is a reland of fa48350aba22b2cb4ee2cbe9a5930c6b68ed894e but with the fix ebitengine/purego#27.
    
    Updates ebitengine/purego#27

--- driver_macos.go
+++ driver_macos.go
@@ -17,7 +17,40 @@
 
 package oto
 
-// #cgo LDFLAGS: -framework AppKit
-import "C"
+import (
+	"unsafe"
+
+	"github.com/ebitengine/purego"
+	"github.com/ebitengine/purego/objc"
+)
 
 const bufferSizeInBytes = 2048
+
+var appkit = purego.Dlopen("/System/Library/Frameworks/AppKit.framework/Versions/Current/AppKit", purego.RTLD_GLOBAL)
+
+// setNotificationHandler sets a handler for sleep/wake notifications.
+func setNotificationHandler() {
+	// Create the Observer object
+	class := objc.AllocateClassPair(objc.GetClass("NSObject\x00"), "OtoNotificationObserver\x00", 0)
+	class.AddMethod(objc.RegisterName("receiveSleepNote:\x00"), objc.NewIMP(setGlobalPause), "v@:@\x00")
+	class.AddMethod(objc.RegisterName("receiveWakeNote:\x00"), objc.NewIMP(setGlobalResume), "v@:@\x00")
+	class.Register()
+
+	observer := objc.ID(class).Send(objc.RegisterName("new\x00"))
+
+	notificationCenter := objc.ID(objc.GetClass("NSWorkspace\x00")).Send(objc.RegisterName("sharedWorkspace\x00")).Send(objc.RegisterName("notificationCenter\x00"))
+	notificationCenter.Send(objc.RegisterName("addObserver:selector:name:object:\x00"),
+		observer,
+		objc.RegisterName("receiveSleepNote:\x00"),
+		// Dlsym returns a pointer to the object so dereference it
+		*(*uintptr)(unsafe.Pointer(purego.Dlsym(appkit, "NSWorkspaceWillSleepNotification"))),
+		0,
+	)
+	notificationCenter.Send(objc.RegisterName("addObserver:selector:name:object:\x00"),
+		observer,
+		objc.RegisterName("receiveWakeNote:\x00"),
+		// Dlsym returns a pointer to the object so dereference it
+		*(*uintptr)(unsafe.Pointer(purego.Dlsym(appkit, "NSWorkspaceDidWakeNotification"))),
+		0,
+	)
+}
